9873347555042dde8cf78da7396a8e581b9a66be
Mehdi <mkianpour@gmail.com>
2015-07-17 17:31:36 +0200

    all attributes to string

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,9 @@
 # General settings for System DSN
 
 default['win-redshift']['name'] = "awsredshift"
-default['win-redshift']['server'] = '172.28.128.11'
-default['win-redshift']['port'] = '5439'  # Must be a string
-default['win-redshift']['database'] = 'dev'
+default['win-redshift']['server'] = "MyServer"
+default['win-redshift']['port'] = "5439"  # Must be a string
+default['win-redshift']['database'] = "dev"
 default['win-redshift']['uid'] = "masteruser"
 default['win-redshift']['pwd'] = "changeme"
 default['win-redshift']['sslmode'] = "prefer"
